156846049cb253c5faad6af6a1284c23181bc3e2
Christopher Tineo <christophertineo02@gmail.com>
2023-08-10 19:06:08 -0400

    fix: add output directory

--- magidoc.mjs
+++ magidoc.mjs
@@ -1,13 +1,10 @@
-// magidoc.mjs
-
 export default {
   introspection: {
-    // type: 'url',
-    // url: 'http://localhost:3000/graphql',
     type: 'sdl',
     paths: ['./src/schema.gql'],
   },
   website: {
     template: 'carbon-multi-page',
+    output: './docs',
   },
 };
