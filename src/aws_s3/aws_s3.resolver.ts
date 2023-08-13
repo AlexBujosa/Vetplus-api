4cba0692e592e8d8f32256f6a70e44e6ac3c213c
Christopher Tineo <christophertineo02@gmail.com>
2023-08-13 12:17:02 -0400

    chore: remove s3 resolver

--- src/aws_s3/aws_s3.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolver } from '@nestjs/graphql';
-import { AwsS3Service } from './aws_s3.service';
-
-@Resolver()
-export class AwsS3Resolver {
-  constructor(private readonly awsS3Service: AwsS3Service) {}
-}
