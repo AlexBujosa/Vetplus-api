76a5540817ece8f0204f48e6f6e99adf3b783f42
Christopher Tineo <christophertineo02@gmail.com>
2023-08-13 12:17:02 -0400

    chore: remove s3 resolver

--- src/aws_s3/aws_s3.module.ts
+++ src/aws_s3/aws_s3.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AwsS3Service } from './aws_s3.service';
-import { AwsS3Resolver } from './aws_s3.resolver';
 
 @Module({
-  providers: [AwsS3Resolver, AwsS3Service]
+  providers: [AwsS3Service],
 })
 export class AwsS3Module {}
