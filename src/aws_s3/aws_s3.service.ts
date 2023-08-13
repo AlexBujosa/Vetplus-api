366fd9cbe6f30d1d1bd99a2f4fca44710f3f9d6f
Christopher Tineo <christophertineo02@gmail.com>
2023-08-13 11:57:28 -0400

    feat: save image function to s3 bucket

--- src/aws_s3/aws_s3.service.ts
+++ src/aws_s3/aws_s3.service.ts
@@ -4,9 +4,24 @@ import AWS from 'aws-sdk';
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-})
+  region: process.env.AWS_S3_REGION,
+});
 
 @Injectable()
 export class AwsS3Service {
-  
+  async savePetImageToS3(file: Express.Multer.File) {
+    const blob = Buffer.from(file.buffer);
+
+    const uploadedImage = await s3
+      .upload({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: file.filename,
+        Body: blob,
+      })
+      .promise();
+
+    const { Location } = uploadedImage;
+
+    return Location;
+  }
 }
