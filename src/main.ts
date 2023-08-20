7134ea94355f63fce64c0c78d13b2af74b4ad87a
Christopher Tineo <christophertineo02@gmail.com>
2023-08-20 17:26:09 -0400

    allow all origins

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomExceptionFilter } from './global/filter/custom-exception.filter';
 import * as passport from 'passport';
-import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new CustomExceptionFilter());
   app.use(passport.initialize());
-  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   app.enableCors({
-    origin: ['http://localhost:5173/'],
+    origin: '*',
   });
   await app.listen(3000);
 }
