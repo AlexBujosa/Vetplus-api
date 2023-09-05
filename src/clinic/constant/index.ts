90449d500f2456fb14f646233e96646b674d1c8a
AlexBujosa <bujosa2014@outlook.com>
2023-09-05 12:41:12 -0400

    feat: :sparkles: GetAllClinicServices

--- src/clinic/constant/index.ts
+++ src/clinic/constant/index.ts
@@ -2,3 +2,16 @@ export enum ClinicResult {
   COMPLETED = 'COMPLETED',
   FAILED = 'FAILED',
 }
+
+export type ServiceResult = {
+  service: {
+    name: string;
+    description: string;
+  };
+} & {
+  id_clinic: string;
+  id_service: string;
+  created_at: Date;
+  updated_at: Date;
+  status: boolean;
+};
