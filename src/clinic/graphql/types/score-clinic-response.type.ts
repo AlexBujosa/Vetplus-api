e1ea841297b85486767ba9af843534059d81b7b0
Christopher Tineo <christophertineo02@gmail.com>
2023-09-09 18:46:24 -0400

    fix: rename clinic response type

--- src/clinic/graphql/types/score-clinic-response.type.ts
+++ src/clinic/graphql/types/score-clinic-response.type.ts
@@ -1,5 +1,5 @@
 import { ObjectType } from '@nestjs/graphql';
-import { AddClinicResponse } from './add-clinic-response.type';
+import { ClinicResponse } from './add-clinic-response.type';
 
 @ObjectType()
-export class ScoreClinicResponse extends AddClinicResponse {}
+export class ScoreClinicResponse extends ClinicResponse {}
