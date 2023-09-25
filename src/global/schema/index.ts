5d5676980aaaeefa6ca5d01a1c391d3171dac1e3
AlexBujosa <bujosa2014@outlook.com>
2023-09-25 19:33:31 -0400

    feat: :sparkles: Adding new way to insert document

--- src/global/schema/index.ts
+++ src/global/schema/index.ts
@@ -1,5 +1,5 @@
 export const telephoneRegex = /^(\d{10}|\d{3}-\d{3}-\d{4})$/;
 export const namesOrSurnamesRegex = /^[A-Za-záéíóúÁÉÍÓÚñÑüÜ\s'-]+$/;
-export const documentRegex = /^(RD\d{7}|\d{3}-\d{7}-\d)$/;
+export const documentRegex = /^(RD\d{7}|\d{3}-\d{7}-\d|\d{11}})$/;
 export const emailRegex =
   /^(?:[a-zA-Z0-9._%+-]+@gmail\.com|[^@]+@(?:hotmail\.com|outlook\.com))$/;
